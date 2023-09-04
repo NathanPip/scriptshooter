d47ae82e39fe6cc07c10d7cd284a638757d7471e
NathanPip <107423247+NathanPip@users.noreply.github.com>
2023-09-04 13:43:14 -0700

    open projects in nvim

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,35 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::os::windows::process::CommandExt;
+use std::process::{Command, Stdio};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hekkl, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn open_nvim(path: &str) {
+    Command::new("D:\\IDE_STUFF\\NeoVim\\bin\\nvim.exe")
+        .raw_arg(path)
+        .creation_flags(0x00000010)
+        .spawn()
+        .expect("failed to open nvim");
+}
+
+#[tauri::command]
+fn open_vs(path: &str) {
+    std::process::Command::new("cmd")
+        .args([path, "code ."])
+        .spawn()
+        .expect("failed to open nvim");
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, open_nvim, open_vs])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import { initialStart } from "./startup";
 import InputModal from "./components/InputModal";
 import { allProjects } from "./state";
+import { Command, open } from "@tauri-apps/api/shell";
 
 export const [showInputModal, setShowInputModal] = createSignal(false);
 export const [inputModalType, setInputModalType] = createSignal("");
@@ -17,31 +18,55 @@ function App() {
   onMount(async () => {
     await initialStart();
     setInitializationRan(true);
-  })
+  });
 
   function addProjectHandler() {
     setShowInputModal(true);
     setInputModalType("new project");
   }
 
+  async function openNvimHandler(path: string) {
+    await invoke("open_nvim", { path: path });
+  }
+
+  async function openCodeHandler(path: string) {
+    await invoke("open_vs", { path: path });
+  }
+
   return (
     <div class="w-full overflow-hidden">
-    <Show when={showInputModal()}>
-      <InputModal type={inputModalType()} />
-    </Show>
+      <Show when={showInputModal()}>
+        <InputModal type={inputModalType()} />
+      </Show>
       <div class="flex gap-2 justify-center py-4 text-2xl border-b-2 border-secondary w-full">
-        <button class="bg-primary bg-opacity-50 px-4 py-2 rounded-md">Open Most Recent</button>
-        <button onClick={addProjectHandler} class="bg-primary bg-opacity-50 px-4 py-2 rounded-md">New Project</button>
-        <button class="bg-primary bg-opacity-50 px-4 py-2 rounded-md">Open Folder</button>
-        <button class="bg-primary bg-opacity-50 px-4 py-2 rounded-md">Config</button>
+        <button class="bg-primary bg-opacity-50 px-4 py-2 rounded-md">
+          Open Most Recent
+        </button>
+        <button
+          onClick={addProjectHandler}
+          class="bg-primary bg-opacity-50 px-4 py-2 rounded-md"
+        >
+          New Project
+        </button>
+        <button class="bg-primary bg-opacity-50 px-4 py-2 rounded-md">
+          Open Folder
+        </button>
+        <button class="bg-primary bg-opacity-50 px-4 py-2 rounded-md">
+          Config
+        </button>
       </div>
-    <div class="w-full">
-      <For each={allProjects()}>
-        { proj =>
-          <button class="text-2xl text-left w-full px-2 pl-4 py-4 bg-primary bg-opacity-0 m-2 border-b-2 border-secondary">{proj.name}</button>
-        }
-      </For>
-    </div>
+      <div class="w-full">
+        <For each={allProjects()}>
+          {(proj) => (
+            <button
+              onClick={() => openNvimHandler(proj.path)}
+              class="text-2xl text-left w-full px-2 pl-4 py-4 bg-primary bg-opacity-0 m-2 border-b-2 border-secondary"
+            >
+              {proj.name}
+            </button>
+          )}
+        </For>
+      </div>
     </div>
   );
 }
